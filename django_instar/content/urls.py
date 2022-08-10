0adcc35145d72100f9c73e4bfa3414c14c4865eb
gukwanglim <100823895+gukwanglim@users.noreply.github.com>
2022-08-10 18:17:45 +0900

    django_instar_6

--- django_instar/content/urls.py
+++ django_instar/content/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UploadFeed, Main, Profile             #, UploadReply, ToggleLike, ToggleBookmark
+from .views import UploadFeed, Main, Profile, UploadReply    #, ToggleLike, ToggleBookmark
 
 urlpatterns = [
     path('upload', UploadFeed.as_view()),
-    # path('reply', UploadReply.as_view()),
+    path('reply', UploadReply.as_view()),
     # path('like', ToggleLike.as_view()),
     # path('bookmark', ToggleBookmark.as_view()),
     path('profile', Profile.as_view()),
--- django_instar/content/views.py
+++ django_instar/content/views.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Feed                          # , Reply, Like, Bookmark
+from .models import Feed, Reply, Like, Bookmark
 from user.models import User
 import os
 from config.settings import MEDIA_ROOT
@@ -18,22 +18,25 @@ class Main(APIView):
 
         for feed in feed_object_list:
             user = User.objects.filter(email=feed.email).first()
-            # reply_object_list = Reply.objects.filter(feed_id=feed.id)
-            # reply_list = []
-            # for reply in reply_object_list:
-                # user = User.objects.filter(email=reply.email).first()
-                # reply_list.append(dict(reply_content=reply.reply_content,
-                                    #    nickname=user.nickname))
+                                                                 # Reply의 feed_id는 실제 feed의 id가 된다.(그 이유는 내가 댓글을 달 피드의 id를 가지고 있어야 그 id에 맞는 피드에 댓글이 올라가기 때문이다.)
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)              # 이런 식으로 한 번 reply_object_list를 만든 다음에 for구문으로 돌린 이유는 아래 UploadReply class에서 email을 가져오지만, 
+            reply_list = []                                                        # 실제로 사용하는 정보는 닉네임이기 때문에 email을 활용하여 필요한 정보를 뽑아내기 위해 사용한다.
+
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=user.nickname))
+
             # like_count=Like.objects.filter(feed_id=feed.id, is_like=True).count()
             # is_liked=Like.objects.filter(feed_id=feed.id, email=email, is_like=True).exists()
             # is_marked=Bookmark.objects.filter(feed_id=feed.id, email=email, is_marked=True).exists()
-            feed_list.append(dict(id=feed.id,
+            feed_list.append(dict(id=feed.id,                                                             # main.html의 108번째 줄에서 feed_id를 {{ feed.id }}로 선언하기 위해서 id=feed.id로 한다.
                                   image=feed.image,
                                   content=feed.content,
                                 #   like_count=like_count,
                                   profile_image=user.profile_image,
                                   nickname=user.nickname,
-                                #   reply_list=reply_list,
+                                  reply_list=reply_list,
                                 #   is_liked=is_liked,
                                 #   is_marked=is_marked
                                   ))
@@ -80,6 +83,61 @@ class Profile(APIView):
                                                                     # like_feed_list=like_feed_list,
                                                                     # bookmark_feed_list=bookmark_feed_list,
                                                                     user=user))
+
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)                             # 적힌 내용들을 받아와 데이터베이스에 저장.
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
+
+
+class ToggleLike(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        favorite_text = request.data.get('favorite_text', True)
+
+        if favorite_text == 'favorite_border':
+            is_like = True
+        else:
+            is_like = False
+        email = request.session.get('email', None)
+
+        like = Like.objects.filter(feed_id=feed_id, email=email).first()
+
+        if like:
+            like.is_like = is_like
+            like.save()
+        else:
+            Like.objects.create(feed_id=feed_id, is_like=is_like, email=email)
+
+        return Response(status=200)
+
+
+class ToggleBookmark(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        bookmark_text = request.data.get('bookmark_text', True)
+        print(bookmark_text)
+        if bookmark_text == 'bookmark_border':
+            is_marked = True
+        else:
+            is_marked = False
+        email = request.session.get('email', None)
+
+        bookmark = Bookmark.objects.filter(feed_id=feed_id, email=email).first()
+
+        if bookmark:
+            bookmark.is_marked = is_marked
+            bookmark.save()
+        else:
+            Bookmark.objects.create(feed_id=feed_id, is_marked=is_marked, email=email)
+
+        return Response(status=200)
 
 
 
@@ -172,56 +230,3 @@ class Profile(APIView):
 #                                                                     user=user))
 
 
-# class UploadReply(APIView):
-#     def post(self, request):
-#         feed_id = request.data.get('feed_id', None)
-#         reply_content = request.data.get('reply_content', None)
-#         email = request.session.get('email', None)
-
-#         Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
-
-#         return Response(status=200)
-
-
-# class ToggleLike(APIView):
-#     def post(self, request):
-#         feed_id = request.data.get('feed_id', None)
-#         favorite_text = request.data.get('favorite_text', True)
-
-#         if favorite_text == 'favorite_border':
-#             is_like = True
-#         else:
-#             is_like = False
-#         email = request.session.get('email', None)
-
-#         like = Like.objects.filter(feed_id=feed_id, email=email).first()
-
-#         if like:
-#             like.is_like = is_like
-#             like.save()
-#         else:
-#             Like.objects.create(feed_id=feed_id, is_like=is_like, email=email)
-
-#         return Response(status=200)
-
-
-# class ToggleBookmark(APIView):
-#     def post(self, request):
-#         feed_id = request.data.get('feed_id', None)
-#         bookmark_text = request.data.get('bookmark_text', True)
-#         print(bookmark_text)
-#         if bookmark_text == 'bookmark_border':
-#             is_marked = True
-#         else:
-#             is_marked = False
-#         email = request.session.get('email', None)
-
-#         bookmark = Bookmark.objects.filter(feed_id=feed_id, email=email).first()
-
-#         if bookmark:
-#             bookmark.is_marked = is_marked
-#             bookmark.save()
-#         else:
-#             Bookmark.objects.create(feed_id=feed_id, is_marked=is_marked, email=email)
-
-#         return Response(status=200)
